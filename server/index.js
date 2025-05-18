a144874c273a90eb077507a49fcb5c5dd4503245
Mich <anamichelle1308@gmail.com>
2025-05-18 00:40:04 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// server/index.js
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API InnovaTube funcionando");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
